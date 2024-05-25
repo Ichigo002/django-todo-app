e89eea5b06d244aed8db7dbb2ba24c42ef8d7fbc
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2024-05-25 18:48:45 +0200

    fix placeholder and flex grid

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from todo_app.models import TodoTask
 
 class TodoTaskForm(forms.ModelForm):
+
+    
+
     class Meta:
         model = TodoTask
         fields = (
@@ -9,3 +12,17 @@ class TodoTaskForm(forms.ModelForm):
             "is_priority",
             "details",
         )
+
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Do the washing . . .'}),
+            'details': forms.Textarea(
+                attrs={
+                        'cols': '18',
+                        'rows': '3',
+                        'style': 'margin-left: 15px',
+                    }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(TodoTaskForm, self).__init__(*args, **kwargs)
+        self.fields['details'].required = False
